b3cc08f582788e6e177dcfe711ec3dca71dd3e2f
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-15 12:19:34 +0200

    implementado routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-     <Header/>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+
+        
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
