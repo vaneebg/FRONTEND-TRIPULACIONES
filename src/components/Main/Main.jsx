7e884631aa7d2b489d40df03c5c3ce73834a1e24
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-15 13:46:26 +0200

    ocultar header en home

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,58 +1,8 @@
-import { UserOutlined,MenuUnfoldOutlined,ReconciliationOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
-import React, { useState } from 'react';
-  
-import {Link} from 'react-router-dom'
-import Routes from './Routes/Routes';
 
 
 const Main = () => {
-    function getItem(label, key, icon, children, type) {
-        return {
-          key,
-          icon,
-          children,
-          label,
-          type,
-        };
-      }
-      
-      const items = [
-        getItem( '', 'sub1', <MenuUnfoldOutlined />, [
-          getItem('', '1', <Link to='/profile'><UserOutlined /></Link>),
-          getItem('', '2', <Link to='/quiz'><ReconciliationOutlined /></Link>),
-        //   getItem('Option 3', '3'),
-        //   getItem('Option 4', '4'),
-        ]),
-    ]
-      
-      const rootSubmenuKeys = ['sub1'];
-      const [openKeys, setOpenKeys] = useState(['sub1']);
-
-      const onOpenChange = (keys) => {
-        const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
-    
-        if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-          setOpenKeys(keys);
-        } else {
-          setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
-        }
-      };      
-
-  return (<>
-  
-  <Menu
-      mode="inline"
-      openKeys={openKeys}
-      onOpenChange={onOpenChange}
-      style={{
-        width: 150,
-      }}
-      items={items}
-    />
-      <Routes/>
-      </>
-    );
+ 
+  return (<h1>Main</h1>)
   };
     
 
