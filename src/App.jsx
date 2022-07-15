b61cb36a33608e95c3825847f75d0d80bc263ad0
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-15 12:22:47 +0200

    creados componentes home y main

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header/Header';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Main from './components/Main/Main';
 
 
 function App() {
@@ -11,8 +13,9 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/main" element={<Main/>}/>
 
-        
         </Routes>
         <Footer/>
       </BrowserRouter>
