637bae74fcabff9fd15e00278ea3cd2eb7333d89
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-15 13:37:17 +0200

    hecho menu desplegable

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,6 @@
-import {
-    AppstoreOutlined,
-    ContainerOutlined,
-    DesktopOutlined,
-    MailOutlined,
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    PieChartOutlined,
-  } from '@ant-design/icons';
-  import { Button, Menu } from 'antd';
-  import React, { useState } from 'react';
+import { UserOutlined,MenuUnfoldOutlined,ReconciliationOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import React, { useState } from 'react';
   
 import {Link} from 'react-router-dom'
 import Routes from './Routes/Routes';
@@ -26,56 +18,38 @@ const Main = () => {
       }
       
       const items = [
-        getItem('Option 1', '1',<Link to='/profile'> <PieChartOutlined /></Link>),
-        getItem('Option 2', '2', <Link to ='/quiz'><DesktopOutlined /></Link>),
-        // getItem('Option 3', '3', <ContainerOutlined />),
-        // getItem('Navigation One', 'sub1', <MailOutlined />, [
-        //   getItem('Option 5', '5'),
-        //   getItem('Option 6', '6'),
-        //   getItem('Option 7', '7'),
-        //   getItem('Option 8', '8'),
-        // ]),
-        // getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-        //   getItem('Option 9', '9'),
-        //   getItem('Option 10', '10'),
-        //   getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
-        // ]),
-      ];
+        getItem( '', 'sub1', <MenuUnfoldOutlined />, [
+          getItem('', '1', <Link to='/profile'><UserOutlined /></Link>),
+          getItem('', '2', <Link to='/quiz'><ReconciliationOutlined /></Link>),
+        //   getItem('Option 3', '3'),
+        //   getItem('Option 4', '4'),
+        ]),
+    ]
       
+      const rootSubmenuKeys = ['sub1'];
+      const [openKeys, setOpenKeys] = useState(['sub1']);
 
-      const [collapsed, setCollapsed] = useState(false);
-  
-      const toggleCollapsed = () => {
-        setCollapsed(!collapsed);
-      };
-
-
+      const onOpenChange = (keys) => {
+        const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+    
+        if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+          setOpenKeys(keys);
+        } else {
+          setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+        }
+      };      
 
   return (<>
   
-      <div
-        style={{
-          width: 256,
-        }}
-      >
-        <Button
-          type="primary"
-          onClick={toggleCollapsed}
-          style={{
-            marginBottom: 16,
-          }}
-        >
-          {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-        </Button>
-        <Menu
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          mode="inline"
-          theme="dark"
-          inlineCollapsed={collapsed}
-          items={items}
-        />
-      </div>
+  <Menu
+      mode="inline"
+      openKeys={openKeys}
+      onOpenChange={onOpenChange}
+      style={{
+        width: 150,
+      }}
+      items={items}
+    />
       <Routes/>
       </>
     );
