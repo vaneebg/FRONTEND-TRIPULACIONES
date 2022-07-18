84a5c7305009fcc445381f1b4f31082bfebd6606
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-18 20:23:38 +0200

    mapeados puntos de interés de cada ruta

--- src/components/Main/Routes/Route/Route.jsx
+++ src/components/Main/Routes/Route/Route.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './Route.scss'
 
 
@@ -20,18 +20,28 @@ const Route = () => {
 
     const { routes } = useSelector((state) => state.routes);
 
-
     const route = routes.map(el => {
+        const points = el.pois.map(point => {
+            return (
+                <Marker position={[point.latitude, point.longitude]}>
+                    <Popup>
+                        {point.name} <br />
+                    </Popup>
+                </Marker>
+            )
+        })
         return (
 
-            <div className="route">
+            <div key={el.id} className="route">
                 <span>Nombre ruta:{el.name}</span> <br />
                 <span>Descripción: {el.description}</span> <br />
                 <span>Dificultad: {el.difficulty}</span> <br />
-
+                <span>Duración del trayecto: {el.duration}</span>
                 <div style={styles.wrapper}>
+
                     <MapContainer style={styles.map} center={[39.46975, -0.37739]} zoom={13}>
                         <TileLayer url={'https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png'} />
+                        {points}
                     </MapContainer>
                 </div>
             </div>
