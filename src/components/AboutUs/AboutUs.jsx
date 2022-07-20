4e9f4c0072e2b66e1fad26c463035c596542b7f9
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-20 11:50:04 +0200

    about us

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,20 +1,45 @@
 
 import { GithubOutlined, LinkedinOutlined } from "@ant-design/icons";
-
+import './AboutUs.scss'
+import imgVane from '../../assets/vane.jpg'
 
 const AboutUs = () => {
-  return (
-    <div>
+  return (<>
+   
         <h2>Aplicación creada por:</h2>
-        <span>Germán MT</span> <a href="https://www.linkedin.com/in/moltorger/"><LinkedinOutlined className='svg' /></a>
-        <a href="https://github.com/Molerog"><GithubOutlined className='svg' /></a><br />
-        <span>Vicente BC</span><a href="https://www.linkedin.com/in/vibarcar/"><LinkedinOutlined className='svg' /></a>
-        <a href="https://github.com/Vincecoorp21"><GithubOutlined className='svg' /></a> <br />
-        <span>Rebeca AS</span><a href="https://www.linkedin.com/in/rebeca-as/"><LinkedinOutlined className='svg' /></a>
-        <a href="https://github.com/RebecaASuesta"><GithubOutlined className='svg' /></a> <br />
-        <span>Vanesa BG</span>  <a href="https://www.linkedin.com/in/vanesa-bg/"><LinkedinOutlined className='svg' /></a>
+        <div className="about">
+        <div className="target">
+
+        <span>Germán MT</span> 
+        <div className="iconsSocial">
+        <a href="https://www.linkedin.com/in/moltorger/"><LinkedinOutlined className='svg' /></a>
+        <a href="https://github.com/Molerog"><GithubOutlined className='svg' /></a>
+        </div>
+        </div>
+        <div className="target">
+        <span>Vicente BC</span>
+        <div className="iconsSocial">
+        <a href="https://www.linkedin.com/in/vibarcar/"><LinkedinOutlined className='svg' /></a>
+        <a href="https://github.com/Vincecoorp21"><GithubOutlined className='svg' /></a> 
+        </div>
+        </div>
+        <div className="target">
+        <span>Rebeca AS</span>
+        <div className="iconsSocial">
+        <a href="https://www.linkedin.com/in/rebeca-as/"><LinkedinOutlined className='svg' /></a>
+        <a href="https://github.com/RebecaASuesta"><GithubOutlined className='svg' /></a> 
+        </div>
+        </div>
+        <div className="target">
+        <img className='imgAuth' src={imgVane} alt="" />
+        <span>Vanesa BG</span>  
+        <div className="iconsSocial">
+        <a href="https://www.linkedin.com/in/vanesa-bg/"><LinkedinOutlined className='svg' /></a>
         <a href="https://github.com/vaneebg"><GithubOutlined className='svg' /></a>
+        </div>
+        </div>
     </div>
+    </>
   )
 }
 
