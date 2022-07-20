8a2813001ab30049599a8a14c87fe881aa2439be
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-20 19:48:10 +0200

    resolviendo merging

--- src/components/Main/Routes/Routes.jsx
+++ src/components/Main/Routes/Routes.jsx
@@ -1,27 +1,17 @@
 import Route from "./Route/Route"
-<<<<<<< HEAD
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from "react-redux"
-import { getAll, reset } from "../../../features/routes/routesSlice"
-import { Skeleton } from "antd"
-=======
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { getAll, reset } from "../../../features/routes/routesSlice";
 import { Skeleton } from "antd";
->>>>>>> 58385c6afc5ef4954f907260eccc83c036f14b0e
 import './Routes.scss'
 
 const Routes = () => {
 
   const { isLoading } = useSelector((state) => state.routes);
 
-<<<<<<< HEAD
   const dispatch = useDispatch();
-=======
 
   const [current, setCurrent] = useState(1);
->>>>>>> 58385c6afc5ef4954f907260eccc83c036f14b0e
 
   const getRoutesAndReset = async () => {
     await dispatch(getAll(current));
@@ -32,10 +22,6 @@ const Routes = () => {
     getRoutesAndReset()
   }, [getAll]);
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 58385c6afc5ef4954f907260eccc83c036f14b0e
   if (isLoading) {
     return <Skeleton active paragraph={{ rows: 20 }} />;
   }
