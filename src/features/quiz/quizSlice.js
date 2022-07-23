595be4f11e66ca39200d8e63a81f300940064d0d
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-23 12:38:19 +0200

    slice completo de quiz funcionando

--- src/features/quiz/quizSlice.js
+++ src/features/quiz/quizSlice.js
@@ -6,6 +6,40 @@ const initialState = {
   
 };
 
+export const createQuiz = createAsyncThunk("quiz/createQuiz", async (data) => {
+    try {
+      return await quizService.createQuiz(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+);
 
+export const getQuiz = createAsyncThunk("quiz/getQuiz", async()=>{
+    try {
+      return await quizService.getQuiz();
+    } catch (error) {
+      console.error(error)
+    }
+  }
+)
+
+
+export const quizSlice = createSlice({
+  name: "quiz",
+  initialState,
+  reducers: {
+
+  },
+  extraReducers: (builder) => {
+    builder.addCase(createQuiz.fulfilled, (state, action) => {
+        state.quiz = action.payload
+     
+      })
+      .addCase(getQuiz.fulfilled,(state, action)=>{
+        state.quiz = action.payload
+      })
+    },
+});
 
 export default quizSlice.reducer;
