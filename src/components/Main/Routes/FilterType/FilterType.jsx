6b337d8027456b7abeb388ea272d3351924c1361
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-25 11:48:48 +0200

    creado componente filter por tipo

---
+++ src/components/Main/Routes/FilterType/FilterType.jsx
@@ -0,0 +1,14 @@
+import './FilterType.scss'
+
+const FilterType = () => {
+  return (
+    <div className='buttonType'>
+    <button>Histórica</button>
+    <button>Turística</button>
+    <button>Literaria</button>
+    <button>Patrimonial</button>
+    </div>
+  )
+}
+
+export default FilterType
