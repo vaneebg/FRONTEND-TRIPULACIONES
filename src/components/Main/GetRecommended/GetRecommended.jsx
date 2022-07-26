aa90081d3fefc299abf276da5802906291866346
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-26 14:26:08 +0200

    link a route detail

--- src/components/Main/GetRecommended/GetRecommended.jsx
+++ src/components/Main/GetRecommended/GetRecommended.jsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRecommended ,reset} from '../../../features/quiz/quizSlice';
 import {  getAllNotPage } from '../../../features/routes/routesSlice';
+import { Link } from 'react-router-dom';
+
 
 
 const GetRecommended = () => {
@@ -20,8 +22,8 @@ console.log(routeFinal)
 
     
     return (<>
-    <h1>Tu ruta recomendada es...</h1>
-    {routeFinal.name}
+    <h1>Tu ruta recomendada es...</h1> <br />
+    <Link to={'/routes/route/' + routeFinal._id}>{routeFinal?.name}</Link>
         </>
     )
 }
